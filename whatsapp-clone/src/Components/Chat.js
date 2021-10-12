e41d8b1c7d5fc29cb1f8792b5aee8b32bfefce2e
jasko255 <jasko255@gmail.com>
2021-10-12 14:34:46 +0200

    messages

--- whatsapp-clone/src/Components/Chat.js
+++ whatsapp-clone/src/Components/Chat.js
@@ -1,6 +1,8 @@
-import { Avatar } from '@material-ui/core'
+import { Avatar, IconButton } from '@material-ui/core'
 import React from 'react'
 import './Chat.css'
+import MoreVertIcon from '@material-ui/icons/MoreVert'
+import { AttachFile, SearchOutlined } from '@material-ui/icons'
 
 
 function Chat() {
@@ -14,11 +16,26 @@ function Chat() {
             <p>Last seen at...</p>
         </div>
         <div className="chat__headerRight">
-            
+        <IconButton>
+        <SearchOutlined />
+          </IconButton> 
+          <IconButton>
+        <AttachFile />
+          </IconButton>
+        <IconButton>
+        <MoreVertIcon />
+        </IconButton>
         </div>
         </div>
         <div className="chat__body">
-
+        
+        <p className="chat__message">
+            <span className="chat__name">
+                Janusz
+            </span>
+            Heey guys
+            <span className="chat__timestamp">3:52pm</span>
+                    </p>
         </div>
         <div className="chat__footer">
 
