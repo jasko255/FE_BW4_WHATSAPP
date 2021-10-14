a766e1aa4b80d0ba060a56930211e22c9a020d27
jasko255 <jasko255@gmail.com>
2021-10-14 15:02:23 +0200

    profile sidebar

--- whatsapp-clone/src/App.js
+++ whatsapp-clone/src/App.js
@@ -1,20 +1,24 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import Sidebar from './Components/Sidebar';
-import Chat from './Components/Chat';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Sidebar from "./Components/Sidebar";
+import Chat from "./Components/Chat";
+import {BrowserRouter as Router, Switch} from "react-router-dom";
 // import MainApp from './Components/MainApp';
-// import Login from './Components/Login';
+import Login from './Components/Login';
 
 function App() {
   return (
     <div className="App">
-    
-    <div className="app_body">
-    <Sidebar />
-    <Chat />
+      <div className="app_body">
+        <Router>
+          
+    <Login />
 
-    </div>
-
+          {/* <Sidebar />
+          
+          <Chat /> */}
+        </Router>
+      </div>
     </div>
   );
 }
--- whatsapp-clone/src/Components/Sidebar.js
+++ whatsapp-clone/src/Components/Sidebar.js
@@ -1,58 +1,95 @@
-import React from 'react'
-import './Sidebar.css'
-import {Avatar, IconButton} from "@material-ui/core"
-import DonutLargeIcon from '@material-ui/icons/DonutLarge'
-import ChatIcon from '@material-ui/icons/Chat'
-import MoreVertIcon from '@material-ui/icons/MoreVert'
-import { SearchOutlined } from '@material-ui/icons'
-import SidebarChat from './SidebarChat'
+import React, { useState } from "react";
+import "./Sidebar.css";
+import { Avatar, IconButton } from "@material-ui/core";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { ArrowBack, Edit, SearchOutlined } from "@material-ui/icons";
+import SidebarChat from "./SidebarChat";
 
 function Sidebar() {
-
-
   const createChat = () => {
-    const roomName = prompt('Please enter name for chat')
+    const roomName = prompt("Please enter name for chat");
 
-    if(roomName){
-        
+    if (roomName) {
     }
+  };
 
-}
+  const [profileBar, setProfileBar] = useState(false);
+  const showProfileBar = () => setProfileBar(!profileBar);
 
   return (
-    <div className='sidebar'>
-      <div className="sidebar__header">
-      <Avatar />
-      <div className="sidebar__headerRight">
-        <IconButton>
-        <DonutLargeIcon />
-          </IconButton> 
+    <>
+      <div className="sidebar">
+        <div className="sidebar__header">
           <IconButton>
-        <ChatIcon  onClick={createChat}/>
+            {/* <Link to='#' className='profile__menu'> */}
+            <Avatar onClick={showProfileBar} />
+            {/* </Link> */}{" "}
           </IconButton>
-        <IconButton>
-        <MoreVertIcon />
-        </IconButton>
-      </div>
-      </div>
-      <div className="sidebar__search">
-        <div className="sidebar__searchContainer">
-
-      <SearchOutlined />
-      <input placeholder='Search or start new chat' type='text' />
+          <div className="sidebar__headerRight">
+            <IconButton>
+              <DonutLargeIcon />
+            </IconButton>
+            <IconButton>
+              <ChatIcon onClick={createChat} />
+            </IconButton>
+            <IconButton>
+              <MoreVertIcon />
+            </IconButton>
+          </div>
+        </div>
+        <div className="sidebar__search">
+          <div className="sidebar__searchContainer">
+            <SearchOutlined />
+            <input placeholder="Search or start new chat" type="text" />
+          </div>
+        </div>
+        <div className="sidebar__chats">
+          {/* <SidebarChat addNewChat/> */}
+          <SidebarChat />
+          <SidebarChat />
+          <SidebarChat />
+          <SidebarChat />
+          <SidebarChat />
         </div>
       </div>
-      <div className="sidebar__chats">
-        {/* <SidebarChat addNewChat/> */}
-        <SidebarChat />
-        <SidebarChat />
-        <SidebarChat />
-        <SidebarChat />
-        <SidebarChat />
 
+      <div
+        className={profileBar ? "profile__section active" : "profile__section"}
+      >
+        <div className="profile__header" onClick={showProfileBar}>
+          <IconButton>
+            {" "}
+            <ArrowBack />{" "}
+          </IconButton>{" "}
+          <p>Profile</p>
+        </div>
+        <div className="profile__body">
+          <div className="avatar__box">
+            <Avatar
+              className="profile__avatar"
+              style={{ width: "140px", height: "140px" }}
+            />
+          </div>
+          <div className="name__field">
+            <h6>Your name</h6>
+            <span> Ubeyt </span> <Edit />
+          </div>
+          <div className="info-field">
+            <span>
+              This is not your username or pin. 
+</span>
+          </div>
+          <div className="about"> <h6> About </h6>
+          <div className="about__input"> <span> Hey there! I am using WhatsApp. </span> <Edit /> </div> </div>
+        </div>
       </div>
-    </div>
-  )
+    </>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
+
+// This name will be visible to 
+// your WhatsApp contacts
